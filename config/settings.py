3708a31a1095b49a89bb35e98aeb2dee9687cc1a
dezhik74 <dezhik@yandex.ru>
2021-04-21 19:47:20 +0300

    0.03 Отладка докер

--- config/settings.py
+++ config/settings.py
@@ -146,8 +146,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_DIR = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [STATIC_DIR]
+# STATIC_DIR = os.path.join(BASE_DIR, 'static')
+# STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
